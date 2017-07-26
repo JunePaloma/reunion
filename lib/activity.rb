58d84778b596b76cfde70ad2ac27a127350bb893
JunePaloma <kborkoski@gmail.com>
2017-07-26 09:08:09 -0600

    Add initialize method and tests

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,9 @@
 class Activity
+  attr_reader :name
+
+  def initialize (activity_name, participants = {})
+    @name = activity_name
+    @participants = participants
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,8 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  def test_activity_has_name
+actv = Activity.new("Brunch")
+assert_equal "Brunch", actv.name
+  end
 end
