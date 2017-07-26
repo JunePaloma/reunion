19d48853c15abb398dd633f2bb1c7de2ee016f68
JunePaloma <kborkoski@gmail.com>
2017-07-26 09:32:58 -0600

    Add all methods and passing tests for iteration one

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,9 +1,24 @@
+require 'pry'
 class Activity
-  attr_reader :name
+  attr_reader :name,
+              :participants,
+              :cost
 
-  def initialize (activity_name, participants = {})
+  def initialize (activity_name)
     @name = activity_name
-    @participants = participants
+    @participants = {}
+    @cost = 0
+  end
+
+
+  def add_participant(name, money)
+    @participants[name] = money
+  end
+
+  def total_cost
+  @cost = @participants.values.inject do |sum, value|
+      sum +value
+    end
   end
 
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,8 +4,39 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+
   def test_activity_has_name
 actv = Activity.new("Brunch")
 assert_equal "Brunch", actv.name
   end
+
+  def test_activity_has_participants
+    actv = Activity.new("Brunch")
+    assert_equal ({}), actv.participants
+  end
+
+  def test_activity_can_have_participants_added
+    actv = Activity.new("Brunch")
+    actv.add_participant("Jim", 20)
+
+    assert_equal ({"Jim" => 20}), actv.participants
+  end
+
+  def test_total_cost
+    actv = Activity.new("Brunch")
+    actv.add_participant("Jim", 20)
+
+    assert_equal 20, actv.total_cost
+  end
+
+  def test_activity_can_have_more_than_one_participant
+    actv = Activity.new("Brunch")
+    actv.add_participant("Jim", 20)
+    actv.add_participant("Joe", 40)
+
+    assert_equal ({"Jim" => 20, "Joe" => 40}), actv.participants
+    assert_equal 60, actv.total_cost
+
+  end
+
 end
