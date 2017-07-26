e3a8520c18ba5900fa51eb0a219d0a35eabad05b
JunePaloma <kborkoski@gmail.com>
2017-07-26 10:00:40 -0600

    Add final stabs at iteration four

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -3,13 +3,22 @@ require './lib/activity'
 
 class Reunion
   attr_reader :name,
-              :activities
+              :activities,
+              :total_cost
   def initialize(name)
     @name = name
     @activities = []
+    @total_cost = []
   end
 
   def add_activity(activity)
     @activities << activity
   end
+
+  def total_cost
+    @activities.each do |activity_obj|
+      @total_cost << activity_obj.cost
+    end
+  end
+
 end
--- test/reunion_test.rb
+++ test/reunion_test.rb
@@ -24,14 +24,15 @@ class ReunionTest < Minitest::Test
 
   end
 
-  # def test_reunion_activity_has_participants
-  #   reunion = Reunion.new("Denver")
-  #   activity_1 = Activity.new("Brunch")
-  #
-  #  activity_1.add_participant("Jim", 20)
-  #  activity_1.add_participant("Joe", 40)
-  #  reunion.add_activity(activity_1)
-  #  reunion.activities
-  #
-  # end
+  def test_reunion_activity_has_a_cost
+    reunion = Reunion.new("Denver")
+    activity_1 = Activity.new("Brunch")
+
+    activity_1 = Activity.new("Brunch")
+    activity_1.add_participant("Jim", 20)
+    activity_1.add_participant("Joe", 40)
+    reunion.add_activity(activity_1)
+
+    assert_equal 60, reunion.total_cost
+  end
 end
